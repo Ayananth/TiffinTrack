39c3b12cef8cd1793812e3d71daf1ef32df84f4b
Ayananth <tsayananth93@gmail.com>
2025-03-26 09:39:36 +0530

    users app

---
+++ TiffinTrack/users/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def register(request):
+    # if request.method == "GET":
+
+
+    return (HttpResponse("<h1> Hello </h1>"))
