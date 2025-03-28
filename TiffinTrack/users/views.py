cbd106de1bef25dd7402e8d4b9029a33cbebc9d5
Ayananth <tsayananth93@gmail.com>
2025-03-28 21:15:15 +0530

    login page created

--- TiffinTrack/users/views.py
+++ TiffinTrack/users/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def register(request):
-    # if request.method == "GET":
-
-
-    return (HttpResponse("<h1> Hello </h1>"))
+def login(request):
+    return render(request, './users/login.html', {})
