dfb37b4f700130b5adf285121b7aebbf607afa30
Ayananth <tsayananth93@gmail.com>
2025-04-04 12:07:39 +0530

    RestaurantProfile model created

--- TiffinTrack/restaurant/models.py
+++ TiffinTrack/restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class RestaurantProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    restaurant_name = models.CharField(max_length=100)
+    owner_name = models.CharField(max_length=100)
+    licence_no = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    address = models.TextField()
+    is_active = models.BooleanField(default=True)
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.restaurant_name
--- TiffinTrack/users/admin.py
+++ TiffinTrack/users/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import CustomUser
+from restaurant.models import RestaurantProfile
 
 # Register your models here.
 admin.site.register(CustomUser)
+admin.site.register(RestaurantProfile)
+
+
