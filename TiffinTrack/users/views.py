379c39c71cf0c5055db0298d9e08705143b29ef2
Ayananth <tsayananth93@gmail.com>
2025-04-20 07:55:56 +0530

    location filter done

--- TiffinTrack/users/views.py
+++ TiffinTrack/users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 # from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
 from restaurant.models import RestaurantProfile
+from accounts.models import UserProfile, Locations
 
 
 
@@ -52,6 +53,38 @@ from restaurant.models import RestaurantProfile
 @login_required(login_url='login')
 def home(request):
     print(request.user)
-    restaurants = RestaurantProfile.objects.filter(is_approved=True)
-    context = {'restaurants': restaurants}
+    all_locations = Locations.objects.all()
+
+    selected_location = "thrissur"
+
+
+    if request.method == "POST":
+        form_id = request.POST.get('form_id')
+        print(f"Form ID: {form_id}")
+        if form_id == 'location_form':
+            print("Location form submitted")
+            location_id = request.POST.get("selected_location")
+            # Fetch the selected location
+            selected_location = Locations.objects.filter(id=location_id).first()
+            if selected_location:
+                # Update user's location in the UserProfile
+                user_profile = UserProfile.objects.filter(user=request.user).first()
+                if user_profile:
+                    user_profile.location = selected_location
+                    user_profile.save()
+                else:
+                    messages.error(request, "User profile not found.")
+            else:
+                selected_location = "thrissur"
+                messages.error(request, "Invalid location selected.")
+
+
+    # Fetch user location (as a Locations instance, not just ID)
+    user_profile = UserProfile.objects.filter(user=request.user).select_related('location').first()
+    location = user_profile.location if user_profile and user_profile.location else selected_location
+    # Filter restaurants
+    restaurants = RestaurantProfile.objects.filter(is_approved=True, location=location)
+
+
+    context = {'restaurants': restaurants, 'location': location, 'all_locations': all_locations}
     return render(request, './users/home.html', context)
