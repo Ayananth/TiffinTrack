6248900d0e8238f7f8e3d8c697a9faeb119be0b2
Ayananth <tsayananth93@gmail.com>
2025-04-25 13:22:49 +0530

    unwanted print removed

--- TiffinTrack/accounts/middleware.py
+++ TiffinTrack/accounts/middleware.py
@@ -8,7 +8,6 @@ class RedirectAuthenticatedUserMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
-        # These are the names of views we want to protect
         self.protected_views = ['send_otp', 'verify_otp','login', 'register', ]
 
     def __call__(self, request):
@@ -16,22 +15,11 @@ class RedirectAuthenticatedUserMiddleware:
         if request.path.startswith('/admin/') or request.path.startswith('/static/'):
             return self.get_response(request)
         if request.user.is_authenticated:
-            # Get the current view name based on the URL being accessed
-            print(request.path_info)    
-            print("path info")
-            current_view_name = resolve(request.path_info).url_name
-
-            print(f"{current_view_name=}")
-            
+            current_view_name = resolve(request.path_info).url_name            
             if current_view_name in self.protected_views:
-                return redirect('user-home')  # Send them to the home page
-
-        # Allow the request to continue to the next middleware/view
+                return redirect('user-home')
         response = self.get_response(request)
         return response
-
-
-
 
 
 class BlockedUserLogoutMiddleware:
@@ -42,8 +30,6 @@ class BlockedUserLogoutMiddleware:
         print("Mildware is running")
         if request.user.is_authenticated:
             if hasattr(request.user, 'is_blocked') and request.user.is_blocked:
-                print(request.user.is_blocked)
-                print(request.user)
                 logout(request)
                 messages.error(request, "Your account has been blocked.")
                 return redirect('login')  # change this to your login or error page name
@@ -54,12 +40,9 @@ class LoginRedirectMessageMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Only for unauthenticated users redirected to login
         print("Login redirect message middleware")
         if not request.user.is_authenticated:
             print("User not authenticated")
             if '/login/' in request.path and 'next' in request.GET:
-                print("Setting messages")
                 messages.error(request, "Please login to continue.")
-        
         return self.get_response(request)
