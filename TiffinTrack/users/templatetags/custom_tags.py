485f88acdc3ca8fea52cd9f7d6adf478cdc2a82b
Ayananth <tsayananth93@gmail.com>
2025-05-17 19:00:33 +0530

    km display in resaturant card

--- TiffinTrack/users/templatetags/custom_tags.py
+++ TiffinTrack/users/templatetags/custom_tags.py
@@ -5,3 +5,18 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+
+@register.filter
+def km(distance, precision=2):
+    """
+    Converts a GeoDjango Distance object to kilometers with optional precision.
+    Usage: {{ restaurant.distance|km }} or {{ restaurant.distance|km:1 }}
+    """
+    if distance is None:
+        return ""
+    try:
+        return f"{distance.km:.{precision}f} km"
+    except AttributeError:
+        return str(distance)
