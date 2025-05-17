a38a855d8243a461e8e1146ab0ec4acffe40d7bd
Ayananth <tsayananth93@gmail.com>
2025-05-17 18:32:40 +0530

    admin side map changed

--- TiffinTrack/accounts/admin.py
+++ TiffinTrack/accounts/admin.py
@@ -3,8 +3,28 @@ from .models import CustomUser, Locations, UserProfile, RestaurantProfile
 
 # Register your models here.
 admin.site.register(CustomUser)
-admin.site.register(RestaurantProfile)
+# admin.site.register(RestaurantProfile)
 admin.site.register(Locations)
-admin.site.register(UserProfile)
 
+
+
+from django.contrib.gis.admin import GISModelAdmin
+from django.contrib import admin
+from .models import RestaurantProfile
+
+@admin.register(RestaurantProfile)
+class RestaurantProfileAdmin(GISModelAdmin):
+    default_lon = 76.1626624  # center longitude
+    default_lat = 10.436608   # center latitude
+    default_zoom = 12         # zoom level
+    map_height = 400          # optional
+    map_width = 800           # optional
+
+@admin.register(UserProfile)
+class RestaurantProfileAdmin(GISModelAdmin):
+    default_lon = 76.1626624  # center longitude
+    default_lat = 10.436608   # center latitude
+    default_zoom = 12         # zoom level
+    map_height = 400          # optional
+    map_width = 800           # optional
 
