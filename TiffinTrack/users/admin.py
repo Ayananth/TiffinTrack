ee580f0f70a9d68e6b6d7a20589df0e99ed123c1
Ayananth <tsayananth93@gmail.com>
2025-05-18 17:42:43 +0530

    admin register models

--- TiffinTrack/users/admin.py
+++ TiffinTrack/users/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from restaurant.models import RestaurantProfile
-from .models import Address, Orders
+from .models import Address, Orders, Wallet, WalletTransaction
 
 # Register your models here.
 
 admin.site.register(Address)
 admin.site.register(Orders)
+admin.site.register(Wallet)
+admin.site.register(WalletTransaction)
+
 
 
 
