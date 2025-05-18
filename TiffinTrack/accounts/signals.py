120ea8bbb1669ea3082e75578ed35b1036581793
Ayananth <tsayananth93@gmail.com>
2025-05-18 17:39:42 +0530

    wallet creation on user registraion

--- TiffinTrack/accounts/signals.py
+++ TiffinTrack/accounts/signals.py
@@ -2,10 +2,12 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import CustomUser, UserProfile, Locations
+from users.models import Wallet
 
 
 @receiver(post_save, sender=CustomUser)
 def create_user_profile(sender, instance, created, **kwargs):
     if created and instance.user_type == 'normal':
-        UserProfile.objects.create(user=instance)    
+        UserProfile.objects.create(user=instance)
+        Wallet.objects.get_or_create(user=instance)    
 
