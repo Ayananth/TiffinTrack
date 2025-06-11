b4203793510527d9c08a7e16b9363a05f6862d94
Ayananth <tsayananth93@gmail.com>
2025-06-11 13:51:32 +0530

    circular import

--- TiffinTrack/coupons/models.py
+++ TiffinTrack/coupons/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-from restaurant.models import RestaurantProfile
+from restaurant.models import
 import random
 import string
 
