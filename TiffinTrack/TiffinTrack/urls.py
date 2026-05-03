1d9c39a8762f3d2cbac9a0098ea735d072a5a8f4
Ayananth <tsayananth93@gmail.com>
2026-05-03 20:32:23 +0530

    error page

--- TiffinTrack/TiffinTrack/urls.py
+++ TiffinTrack/TiffinTrack/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import error_views
@@ -29,6 +29,7 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("coupons/", include("coupons.urls")),
     path("payments/", include("payments.urls")),
+    re_path(r"^.*$", error_views.page_not_found, {"exception": None}),
 ]
 
 if settings.DEBUG:
